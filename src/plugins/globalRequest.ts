fc9594224b50067845067c33a0050ff83602f753
yang-zihe1235 <972849883@qq.com>
2024-09-05 17:23:09 +0800

    域名版本,完全上线

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -2,17 +2,17 @@
  * request 网络请求工具
  * 更详细的 api 文档: https://github.com/umijs/umi-request
  */
-import {extend} from 'umi-request';
-import {message} from "antd";
-import {history} from "@@/core/history";
-import {stringify} from "querystring";
+import { extend } from 'umi-request';
+import { message } from 'antd';
+import { history } from '@@/core/history';
+import { stringify } from 'querystring';
 
 /**
  * 配置request请求时的默认参数
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : undefined
+  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.yangbo666.site' : undefined,
   // requestType: 'form',
 });
 
@@ -20,7 +20,7 @@ const request = extend({
  * 所以请求拦截器
  */
 request.interceptors.request.use((url, options): any => {
-  console.log(`do request url = ${url}`)
+  console.log(`do request url = ${url}`);
 
   return {
     url,
@@ -48,7 +48,7 @@ request.interceptors.response.use(async (response, options): Promise<any> => {
       }),
     });
   } else {
-    message.error(res.description)
+    message.error(res.description);
   }
   return res.data;
 });
